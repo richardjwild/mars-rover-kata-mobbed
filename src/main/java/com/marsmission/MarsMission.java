17bca4355bea235b405f61fc73f1a97d0ad993d2
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-11-06 11:25:54 +0000

    Refactored turn logic

--- src/main/java/com/marsmission/MarsMission.java
+++ src/main/java/com/marsmission/MarsMission.java
@@ -3,15 +3,21 @@ package com.marsmission;
 public class MarsMission {
     public String execute(String transmission) {
         String[] tokens = transmission.split("\n");
-        if (tokens.length > 2 && !tokens[2].isEmpty()) {
-            if (tokens[2].equals("L"))
-                return "1 1 W";
-            return "1 1 E";
-        }
+
+        if (tokens.length > 2 && !tokens[2].isEmpty())
+            return turn(tokens[2]);
+
         String positions = "";
         for (int i = 1; i < tokens.length; i += 2) {
             positions += tokens[i] + "\n";
         }
         return positions.trim();
     }
+
+    private String turn(String token) {
+        if (token.equals("L"))
+            return "1 1 W";
+        return "1 1 E";
+    }
+
 }
