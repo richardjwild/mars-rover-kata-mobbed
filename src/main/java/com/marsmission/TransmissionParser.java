0982aca8f346075615b85bd2dc1812aaa62a7860
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-11-06 15:04:59 +0000

    Extract parse instructions

--- src/main/java/com/marsmission/TransmissionParser.java
+++ src/main/java/com/marsmission/TransmissionParser.java
@@ -3,9 +3,7 @@ package com.marsmission;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.marsmission.Instruction.MOVE;
-import static com.marsmission.Instruction.TURN_LEFT;
-import static com.marsmission.Instruction.TURN_RIGHT;
+import static com.marsmission.Instruction.*;
 
 public class TransmissionParser {
 
@@ -14,21 +12,8 @@ public class TransmissionParser {
         String[] lines = input.split("\n", -1);
 
         for (int i = 1; i < lines.length; i += 2) {
-            Coordinate coordinate = getCoordinates(lines, i);
-            List<Instruction> instructions = new ArrayList<>();
-            if (i + 1 < lines.length) {
-                switch (lines[i + 1]) {
-                    case "L":
-                        instructions.add(TURN_LEFT);
-                        break;
-                    case "R":
-                        instructions.add(TURN_RIGHT);
-                        break;
-                    case "M":
-                        instructions.add(MOVE);
-                        break;
-                }
-            }
+            Coordinate coordinate = parseCordinates(lines, i);
+            List<Instruction> instructions = parseInstruction(lines, i);
             transmission.roverDefinitions.add(
                 new RoverDefinition(coordinate, instructions));
         }
@@ -36,11 +21,29 @@ public class TransmissionParser {
         return transmission;
     }
 
-    private Coordinate getCoordinates(String[] lines, int roverInputLine) {
+    private Coordinate parseCordinates(String[] lines, int roverInputLine) {
         String coordinates = lines[roverInputLine].substring(0, 4);
         String[] rawCoordinates = coordinates.split(" ");
         int x = Integer.parseInt(rawCoordinates[0]);
         int y = Integer.parseInt(rawCoordinates[1]);
         return new Coordinate(x, y);
+    }
+
+    private List<Instruction> parseInstruction(String[] lines, int i) {
+        List<Instruction> instructions = new ArrayList<>();
+        if (i + 1 < lines.length) {
+            switch (lines[i + 1]) {
+                case "L":
+                    instructions.add(TURN_LEFT);
+                    break;
+                case "R":
+                    instructions.add(TURN_RIGHT);
+                    break;
+                case "M":
+                    instructions.add(MOVE);
+                    break;
+            }
+        }
+        return instructions;
     }
 }
