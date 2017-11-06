9efd3f802604d9467e73648151681291b5f67234
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-11-06 10:44:36 +0000

    Reports two rovers initial positions when no instructions are given

--- src/main/java/com/marsmission/MarsMission.java
+++ src/main/java/com/marsmission/MarsMission.java
@@ -2,6 +2,10 @@ package com.marsmission;
 
 public class MarsMission {
     public String execute(String transmission) {
-        return transmission.split("\n")[1];
+        String[] tokens = transmission.split("\n");
+        if (tokens.length <= 2) {
+            return tokens[1];
+        }
+        return tokens[1] + "\n" + tokens[3];
     }
 }
--- src/test/java/com/marsmission/MarsMissionShould.java
+++ src/test/java/com/marsmission/MarsMissionShould.java
@@ -24,4 +24,18 @@ public class MarsMissionShould {
 
         assertEquals(expectedRoverPosition, actualRoverPosition);
     }
+
+    @Test
+    @Parameters({
+        "5 5\n1 1 N\n\n2 2 N\n | 1 1 N\n2 2 N"
+    })
+    public void
+    report_rovers_initial_position_when_no_instructions_are_provided(
+        String transmission, String expectedRoversPosition) {
+        MarsMission mission = new MarsMission();
+
+        String actualRoverPosition = mission.execute(transmission);
+
+        assertEquals(expectedRoversPosition, actualRoverPosition);
+    }
 }
