92bb6f5b4ae8ee2bb2942ea63ecef6f29e7c3fd5
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-11-06 10:37:25 +0000

    Refactored tests to be parametrized

--- src/main/java/com/marsmission/MarsMission.java
+++ src/main/java/com/marsmission/MarsMission.java
@@ -2,6 +2,6 @@ package com.marsmission;
 
 public class MarsMission {
     public String execute(String transmission) {
-        return "1 1 N";
+        return transmission.split("\n")[1];
     }
 }
--- src/test/java/com/marsmission/MarsMissionShould.java
+++ src/test/java/com/marsmission/MarsMissionShould.java
@@ -1,18 +1,27 @@
 package com.marsmission;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class MarsMissionShould {
 
-    @Test public void
-    report_rover_initial_position_when_no_instructions_are_provided() {
+    @Test
+    @Parameters({
+        "5 5\n1 1 N\n | 1 1 N",
+        "5 5\n3 3 N\n | 3 3 N"
+    })
+    public void
+    report_rover_initial_position_when_no_instructions_are_provided(
+        String transmission, String expectedRoverPosition) {
         MarsMission mission = new MarsMission();
-        String transmission = "5 5\n1 1 N\n";
 
         String actualRoverPosition = mission.execute(transmission);
 
-        assertEquals("1 1 N", actualRoverPosition);
+        assertEquals(expectedRoverPosition, actualRoverPosition);
     }
 }
