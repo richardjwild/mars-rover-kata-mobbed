c88a6483c78fe596d6f2223f4cb16985518e34d3
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2017-11-06 11:55:31 +0000

    Refactored rover turning

--- src/main/java/com/marsmission/MarsMission.java
+++ src/main/java/com/marsmission/MarsMission.java
@@ -4,19 +4,26 @@ public class MarsMission {
     public String execute(String transmission) {
         String[] tokens = transmission.split("\n", -1);
 
-        if (tokens.length > 4 && !tokens[2].isEmpty() && !tokens[4].isEmpty()) {
-            return "" +
-                tokens[1].substring(0, 4) + turn(tokens[2]) + "\n" +
-                tokens[3].substring(0, 4) + turn(tokens[4]);
-        }
-        if (tokens.length > 2 && !tokens[2].isEmpty())
-            return tokens[1].substring(0, 4) + turn(tokens[2]);
-
         String positions = "";
         for (int i = 1; i < tokens.length; i += 2) {
-            positions += tokens[i] + "\n";
+            if (hasInstructions(tokens, i))
+                positions += getBearingAndPosition(tokens, i) + "\n";
+            else
+                positions += tokens[i] + "\n";
         }
         return positions.trim();
+    }
+
+    private boolean hasInstructions(String[] tokens, int roverInputLine) {
+        return tokens.length > roverInputLine + 1 && !tokens[roverInputLine + 1].isEmpty();
+    }
+
+    private String getBearingAndPosition(String[] tokens, int roverInputLine) {
+        return getInitialCoordinates(tokens, roverInputLine) + turn(tokens[roverInputLine + 1]);
+    }
+
+    private String getInitialCoordinates(String[] tokens, int roverInputLine) {
+        return tokens[roverInputLine].substring(0, 4);
     }
 
     private String turn(String token) {
